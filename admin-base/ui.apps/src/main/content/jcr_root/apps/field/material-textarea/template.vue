244ffb031268ecbdc1d1049e9790d21898c3a4ae
Fabian Haupt <fabianhaupt@metaproc.com>
2019-08-15 14:49:45 +0200

    METDEV-1661 added characterCounter for the textarea

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-textarea/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-textarea/template.vue
@@ -1,27 +1,35 @@
 <template>
-	<div class="wrapper test">
+	<div class="wrapper">
 		<div v-if="!schema.preview" class="input-field">
-			<textarea 
-				ref="textarea"
-				class="form-control materialize-textarea" 
-				v-model="value" 
-				v-bind:style="{minHeight: `${1.1 * schema.rows + 2.2}em`, maxHeight: `${1.1 * schema.rows + 2.2}em`}"
-				:id="getFieldID(schema)" 
-				:disabled="disabled" 
-				:maxlength="schema.max" 
-				:minlength="schema.min" 
-				:placeholder="schema.placeholder" 
-				:readonly="schema.readonly" 
-				:name="schema.inputName">
+			<textarea
+					ref="textarea"
+					class="form-control materialize-textarea"
+					v-model="value"
+					v-bind:style="{minHeight: `${1.1 * schema.rows + 2.2}em`, maxHeight: `${1.1 * schema.rows + 2.2}em`}"
+					:id="getFieldID(schema)"
+					:disabled="disabled"
+					:maxlength="schema.max"
+					:minlength="schema.min"
+					:placeholder="schema.placeholder"
+					:readonly="schema.readonly"
+					:name="schema.inputName">
 			</textarea>
 		</div>
 		<p v-else>{{value}}</p>
+		<p v-if="schema.charCount">{{ characterCount }}</p>
 	</div>
 </template>
 
-<script>	
+<script>
 	export default {
 		mixins: [ VueFormGenerator.abstractField ],
+
+		data() {
+			return {
+				characterCount: 0
+			}
+		},
+
 		mounted() {
 			// $(this.$refs.textarea).trigger('autoresize');
 		},
@@ -29,6 +37,9 @@
 			// if( document.activeElement != this.$refs.textarea ){
 			// 	$(this.$refs.textarea).trigger('autoresize');
 			// }
+			console.log(this.$refs.textarea.value.length);
+			this.characterCount = this.$refs.textarea.value.length;
+			console.log(this.characterCount);
 		}
 	}
 </script>
