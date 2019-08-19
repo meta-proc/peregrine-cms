8118bfe9dc6ad3dccd661db0027e3891292144f0
Fabian Haupt <fabianhaupt@metaproc.com>
2019-08-19 12:46:04 +0200

    METDEV-1661 added new textinput editor element

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/textinput/template.vue
@@ -0,0 +1,49 @@
+<template>
+	<input class="wrapper">
+		<input
+			ref="textinput"
+			class="form-control"
+			v-model="value"
+			:id="getFieldID(schema)"
+			:disabled="disabled"
+			:placeholder="schema.placeholder"
+			type="text">
+		</input>
+		<input></input>
+		<p ref="cc" v-if="schema.charCount">{{ countCharacters() }}</p>
+</template>
+
+<script>
+	export default {
+		mixins: [ VueFormGenerator.abstractField ],
+
+		data() {
+			return {
+				characterCount: 0
+			}
+		},
+
+		mounted() {
+			// $(this.$refs.textarea).trigger('autoresize');
+			this.characterCount = this.$refs.textarea.value.length;
+		},
+
+		methods: {
+			countCharacters() {
+				let textArea = this.$refs.textarea;
+				if(!textArea) {
+					this.characterCount = 0;
+				} else {
+					this.characterCount = this.$refs.textarea.value.length;
+				}
+				return this.characterCount;
+			}
+		},
+
+		updated() {
+			// if( document.activeElement != this.$refs.textarea ){
+			// 	$(this.$refs.textarea).trigger('autoresize');
+			// }
+		}
+	}
+</script>
