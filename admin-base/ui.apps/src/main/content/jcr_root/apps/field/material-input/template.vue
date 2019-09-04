9042dec147345bedf481db56c099482c7b814052
Niklas Fonseca Luis <niklas.fonseca-luis@cybercon.de>
2019-09-04 10:40:32 +0200

    METDEV-1564: Extend material-input
    Extend material-input to allow min and max values either as "min" or
    "max" or as "interval" in format "[min,max]" to include provided value
    or "]min,max[" to exclude provided values a mix is possible too

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-input/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-input/template.vue
@@ -1,69 +1,112 @@
 <template>
-	<div class="wrapper">
-		<div v-if="!schema.preview" class="input-field">
-			<input
-				ref="input"
-        type="text"
-				class="form-control materialize-input"
-				v-model="value"
-				v-bind:style="{minHeight: `${1.1 * schema.rows + 2.2}em`, maxHeight: `${1.1 * schema.rows + 2.2}em`}"
-				:id="getFieldID(schema)"
-        :min="schema.min"
-        :max="schema.max"
-				:disabled="disabled"
-        @keypress="allowed()"
+  <div class="wrapper">
+    <div
+      v-if="!schema.preview"
+      class="input-field"
+    >
+      <input
+        ref="input"
+        :disabled="disabled"
+        :id="getFieldID(schema)"
+        :max="getMinMax('max')"
+        :min="getMinMax('min')"
+        :name="schema.inputName"
         :pattern="schema.pattern"
-				:placeholder="schema.placeholder"
-				:readonly="schema.readonly"
-				:name="schema.inputName">
-			</input>
-		</div>
-		<p v-else>{{value}}</p>
-	</div>
+        :placeholder="schema.placeholder"
+        :readonly="schema.readonly"
+        @keydown="allowed()"
+        @blur="minMax()"
+        v-bind:style="{minHeight: `${1.1 * schema.rows + 2.2}em`, maxHeight: `${1.1 * schema.rows + 2.2}em`}"
+        v-model="value"
+        class="form-control materialize-input"
+        type="text"
+      />
+      <p style="color: #607d8b">
+        min: {{ getMinMax('min') }}, max: {{ getMinMax('max') }}
+      </p>
+    </div>
+    <p v-else>
+      {{ value }}
+    </p>
+  </div>
 </template>
 
 <script>
-	export default {
-		mixins: [ VueFormGenerator.abstractField ],
-		mounted() {
-		},
-		updated() {
-		},
-    methods: {
-		  isNumber: function(evt) {
-        evt = (evt) ? evt : window.event;
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
-          evt.preventDefault();
-        } else {
-          return true;
+export default {
+  mixins: [VueFormGenerator.abstractField],
+  mounted() {
+  },
+  updated() {
+  },
+  methods: {
+    getMinMax(s){
+      switch(s){
+      case "min":
+        if( this.schema.min ) return this.schema.min;
+        if( this.schema.interval ){
+          let interval = this.parseInterval(this.schema.interval);
+          return interval.min;
         }
-      },
-		  isInteger: function(evt) {
-        evt = (evt) ? evt : window.event;
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-          evt.preventDefault();
-        } else {
-          return true;
+        break;
+      case "max":
+        if( this.schema.max ) return this.schema.max;
+        if( this.schema.interval ){
+          let interval = this.parseInterval(this.schema.interval);
+          return interval.max;
         }
-      },
-		  allowed(){
-        switch(this.schema.allowed){
-        case "number":
-          return this.isNumber();
-          break;
-        case "integer":
-          return this.isInteger();
-          break;
-        case "double":
-          return this.isNumber();
-          break;
-        default:
-          return () => {};
-          break;
-        }
+        break;
+      default:
+        return 0;
+      }
+    },
+    parseInterval(interval){
+      let i = interval.split(",");
+      if( i.length < 2 ) return null;
+      let min = parseInt(i[0].substring(1), 10);
+      let max = parseInt(i[1].substring(0, i[1].length-1), 10);
+      let minBorder = (i[0].charAt(0) === "[") ? 0 : 1;
+      let maxBorder = (i[1].charAt(i[1].length-1) === "]") ? 0 : 1;
+      return {
+        "min": min + minBorder,
+        "max": max - maxBorder
+      }
+    },
+    isNumber: function (evt) {
+      evt = (evt) ? evt : window.event;
+      let charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 96 || charCode > 105)) && charCode !== 46) {
+        evt.preventDefault();
+      } else {
+        return true;
+      }
+    },
+    isInteger: function (evt) {
+      evt = (evt) ? evt : window.event;
+      let charCode = (evt.which) ? evt.which : evt.keyCode;
+      if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 96 || charCode > 105)) {
+        evt.preventDefault();
+      } else {
+        return true;
+      }
+    },
+    minMax(){
+      let min = this.getMinMax('min');
+      let max = this.getMinMax('max');
+      if( this.value > max ) this.value = max;
+      if( this.value < min ) this.value = min;
+    },
+    allowed() {
+      switch (this.schema.allowed) {
+      case 'number':
+        return this.isNumber();
+      case 'integer':
+        return this.isInteger();
+      case 'double':
+        return this.isNumber();
+      default:
+        return () => {};
       }
     }
-	}
+  }
+};
 </script>
